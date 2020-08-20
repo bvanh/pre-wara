16731f7fa4fdebb8a643ff6628436985536fb7d3
vietahn <bv.anh194@gmail.com>
2020-08-21 06:21:33 +0700

    g

--- src/components/page2/footer.js
+++ src/components/page2/footer.js
@@ -13,11 +13,23 @@ const Footer = () => {
               <img src={imgPage2["logo_lussom.png"]} className='footer-logo'/>
             </div>
             <h3>Wara</h3>
-            <p>Vận hành bởi Lussom.Co.Ltd</p>
-            <p>Địa chỉ: 27</p>
+            <p>Vận hành bởi Lussom Co,.Ltd</p>
+            <p>Địa chỉ: số 27 Trần Duy Hưng, Yên Hoà, Cầu Giấy, Hà Nội</p>
             <p>Email: lussom.co@gmail.com</p>
           </Col>
-          <Col></Col>
+          <Col span={8}>
+          <iframe
+            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2F3Q-Zombie-109545390467375%2F&tabs=timeline&width=340&height=70&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=630380541117442"
+            width="100%"
+            height="80"
+            // style={{}}
+            scrolling="no"
+            frameborder="0"
+            allowTransparency="true"
+            allow="encrypted-media"
+            data-show-facepile="true"
+          ></iframe>
+          </Col>
         </Row>
       </Col>
     </Row>
