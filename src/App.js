e5a7ba3d77c79446b7faadfc84b412812f66c6aa
vietahn <bv.anh194@gmail.com>
2020-08-25 22:00:41 +0700

    add rotate

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import PageBrowser from "./components/pageBrowser";
 import { withOrientationChange, isMobileOnly } from "react-device-detect";
 import "./App.scss";
+import { imgApp } from "./ultils/importImg";
 
 function App(props) {
   const { isLandscape, isPortrait } = props;
@@ -14,7 +15,14 @@ function App(props) {
       );
     }
     if (isPortrait) {
-      return <h2 className='alert-rotate'>Xoay màn hình để hiện thị nội dung!</h2>;
+      return (
+        <div className="bg-rotate">
+          <img src={imgApp['rotate-phone.png']} className='rotate-phone'/>
+          <h3 className="alert-rotate">
+            Vui lòng quay ngang thiết bị để bắt đầu!
+          </h3>
+        </div>
+      );
     }
   }
   return (
